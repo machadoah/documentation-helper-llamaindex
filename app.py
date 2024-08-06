a7a336d258a8fe0b282b9d60e632656896bdb781
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-08-06 11:55:42 -0300

    :sparkles: feat: Construindo front-end com Streamlit

--- app.py
+++ app.py
@@ -2,23 +2,62 @@ from dotenv import load_dotenv
 from pinecone import Pinecone
 from llama_index.core import VectorStoreIndex
 from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.core.chat_engine.types import ChatMode
+import streamlit as st
 import os
 
 
 load_dotenv()
 
-pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
-if __name__ == "__main__":
-    print("RAG...")
+@st.cache_resource(show_spinner=False)
+def get_index() -> VectorStoreIndex:
 
+    pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
     index_name = "documentation-helper-llamaindex"
     pinecone_index = pc.Index(name=index_name)
     vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
-    index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
-    query = "What is a LlamaIndex query engine?"
-    query_engine = index.as_query_engine()
+    return VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
-    response = query_engine.query(query)
-    print(response)
+
+index = get_index()
+
+if "chat_engine" not in st.session_state.keys():
+    st.session_state["chat_engine"] = index.as_chat_engine(
+        chat_mode=ChatMode.CONTEXT, verbose=True
+    )
+
+
+st.set_page_config(
+    page_title="Chat with LlamaIndex docs, powered by LlamaIndex",
+    page_icon="🦙",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+st.title("Chat with LlamaIndex docs 💬 🦙")
+
+if "messages" not in st.session_state.keys():
+    st.session_state["messages"] = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about LlamaIndex open sorce python library?",
+        }
+    ]
+
+if prompt := st.chat_input("Your question"):
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if st.session_state["messages"][-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state["chat_engine"].chat(message=prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state["messages"].append(message)
